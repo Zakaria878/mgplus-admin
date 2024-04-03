b075cf3cd7ef9e668ce937cd4fe252ed83330d3d
Zakaria878 <hzakaria3311@gmail.com>
2024-04-03 12:57:02 +0600

    first commit

---
+++ components/layout/LeftSideBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftSideBar = () => {
+  return (
+    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden">
+      LeftSideBar
+    </div>
+  );
+};
+
+export default LeftSideBar;
